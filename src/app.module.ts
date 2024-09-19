e8c4ec30b4ed9c3cc65052f78b1f5ce37e154974
franklin rodriguez <franklinserif@gmail.com>
2024-09-19 08:56:46 -0400

    feat(app.module.ts): add typeorm and database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from '@config/configuration';
+import { User } from '@users/entities/user.entity';
 
 @Module({
+  imports: [
+    UsersModule,
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
