9221ef032ec23c922d8cb3cc17c6704ae500ad44
franklin rodriguez <franklinserif@gmail.com>
2024-09-19 10:28:40 -0400

    ci(jest): add new jest configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
