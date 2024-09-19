c27b262733f7421b3584f3aee32e9c733cd401ec
franklin rodriguez <franklinserif@gmail.com>
2024-09-19 08:57:03 -0400

    feat(module): add users.module.ts

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '@users/users.service';
+import { UsersController } from '@users/users.controller';
+import { User } from '@users/entities/user.entity';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],
+})
+export class UsersModule {}
