2e76d58c2d434be3d5a9acb1aab7451a7b4a26fd
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 09:34:45 -0400

    feat(links.module): imports VisitsModule

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -3,10 +3,11 @@ import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Link } from '@links/entities/link.entity';
+import { VisitsModule } from '@visits/visits.module';
 
 @Module({
   controllers: [LinksController],
   providers: [LinksService],
-  imports: [TypeOrmModule.forFeature([Link])],
+  imports: [TypeOrmModule.forFeature([Link]), VisitsModule],
 })
 export class LinksModule {}
