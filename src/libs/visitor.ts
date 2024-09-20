24b037d3bf4df3f39983747bf7309dc35900b47c
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 09:29:59 -0400

    feat(libs): rename and add new feature for users libs

---
+++ src/libs/visitor.ts
@@ -0,0 +1,54 @@
+import { Request } from 'express';
+import * as UAParser from 'ua-parser-js';
+import axios from 'axios';
+import { IPAPI_HOST } from '@shared/constants/ipapi';
+import { IpapiResponse } from '@shared/interfaces/ipapi';
+
+function getIp(req: Request) {
+  return (
+    req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  );
+}
+
+async function getGeo(req: Request) {
+  const ethernetProtocolDir = getIp(req);
+
+  let ip: any =
+    ethernetProtocolDir === '::1' ? '38.171.85.131' : ethernetProtocolDir;
+
+  try {
+    const geoLocationInformation = await axios.get<IpapiResponse>(
+      `${IPAPI_HOST}/${ip}/json`,
+    );
+
+    ip = Array.isArray(ip) ? ip.join('') : ip;
+
+    return {
+      ip: ip,
+      location: geoLocationInformation.data.city,
+      country: geoLocationInformation.data.country_name,
+    };
+  } catch (error) {
+    return { ip: ip, location: '', country: '' };
+  }
+}
+
+function getUserAgentInformation(req: Request) {
+  const userAgent = req.headers['user-agent'];
+
+  const parser = new UAParser();
+
+  const visitor = parser.setUA(userAgent).getResult();
+
+  return {
+    browser: visitor.browser.name,
+    os: visitor.os,
+  };
+}
+
+export async function getVisitorInformation(req: Request) {
+  return {
+    geo: await getGeo(req),
+    userAgent: getUserAgentInformation(req),
+  };
+}
