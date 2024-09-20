b740ad1176f275bc3902f5ee041bb7ebb1ab1eff
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 02:29:43 -0400

    feat(controllers): add links.controller implementation

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { LinksService } from './links.service';
 import { CreateLinkDto } from './dto/create-link.dto';
 import { UpdateLinkDto } from './dto/update-link.dto';
@@ -19,16 +27,16 @@ export class LinksController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.linksService.findOne(+id);
+    return this.linksService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
-    return this.linksService.update(+id, updateLinkDto);
+    return this.linksService.update(id, updateLinkDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.linksService.remove(+id);
+    return this.linksService.remove(id);
   }
 }
