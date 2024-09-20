dadd399b0aa989e7b0099e24650fb66bdb1b7841
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 09:33:57 -0400

    refactor(visits.controller): remove all endpoints

--- src/visits/visits.controller.ts
+++ src/visits/visits.controller.ts
@@ -1,41 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { VisitsService } from '@visits/visits.service';
-import { CreateVisitDto, UpdateVisitDto } from '@visits/dto';
 
 @Controller('visits')
 export class VisitsController {
   constructor(private readonly visitsService: VisitsService) {}
-
-  @Post()
-  create(@Body() createVisitDto: CreateVisitDto) {
-    return this.visitsService.create(createVisitDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.visitsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.visitsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVisitDto: UpdateVisitDto) {
-    return this.visitsService.update(+id, updateVisitDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.visitsService.remove(+id);
-  }
 }
