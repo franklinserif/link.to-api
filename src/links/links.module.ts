5b886c42ae33bcc45f5db1bd91b71ae7be314292
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 02:30:06 -0400

    feat(links.module): add typeorm and entitiy configuration

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from '@links/entities/link.entity';
 
 @Module({
   controllers: [LinksController],
   providers: [LinksService],
+  imports: [TypeOrmModule.forFeature([Link])],
 })
 export class LinksModule {}
