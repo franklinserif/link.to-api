eceaed49c6b47157ce73bfc0a73d04a8929b3cb6
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 07:06:56 -0400

    feat(create-visit.dto): add a better implementation with all properties

--- src/visits/dto/create-visit.dto.ts
+++ src/visits/dto/create-visit.dto.ts
@@ -1,1 +1,28 @@
-export class CreateVisitDto {}
+import { IsIP, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateVisitDto {
+  @IsString()
+  @IsOptional()
+  browser: string;
+
+  @IsString()
+  @IsOptional()
+  country: string;
+
+  @IsString()
+  @IsOptional()
+  location: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIP()
+  ip: string;
+
+  @IsNumber()
+  @IsOptional()
+  view: number;
+
+  @IsString()
+  @IsOptional()
+  os: string;
+}
