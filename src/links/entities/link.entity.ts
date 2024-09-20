4befdfa5726943983fe9f8a84df22d2d3785a8d7
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 02:18:54 -0400

    feat(entities): add Link entity implementation

--- src/links/entities/link.entity.ts
+++ src/links/entities/link.entity.ts
@@ -1,1 +1,30 @@
-export class Link {}
+import { User } from '@users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('Links')
+export class Link {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text', name: 'url_original' })
+  urlOriginal: string;
+
+  @Column({ type: 'text', name: 'short_URL', unique: true })
+  shortURL: string;
+
+  @ManyToOne(() => User, (users) => users.links, { nullable: true })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
