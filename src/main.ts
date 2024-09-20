10d6e01b34122834ff8104b28b159e46d9148e7f
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 06:29:46 -0400

    refactor(main): exclude links/:id route from global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'links/:id', method: RequestMethod.GET }],
+  });
   await app.listen(3000);
 }
 bootstrap();
