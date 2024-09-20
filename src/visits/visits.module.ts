50b167322ce2ebd886a4e7b34fca5ad1980cf39b
franklin rodriguez <franklinserif@gmail.com>
2024-09-20 07:08:13 -0400

    feat(vists.module): add typeorm module condiguration

--- src/visits/visits.module.ts
+++ src/visits/visits.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { VisitsService } from './visits.service';
-import { VisitsController } from './visits.controller';
+import { VisitsService } from '@visits/visits.service';
+import { VisitsController } from '@visits/visits.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Visit } from '@visits/entities/visit.entity';
 
 @Module({
   controllers: [VisitsController],
   providers: [VisitsService],
+  imports: [TypeOrmModule.forFeature([Visit])],
 })
 export class VisitsModule {}
