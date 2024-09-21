90f617966de13ac5fd8f809e9e95e363cb867907
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:44:56 -0400

    feat(dto): add signin.dto

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,11 +1,21 @@
-import { IsString, Matches, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class LoginDto {
+export class SignInDto {
   @IsString()
+  @MinLength(2)
+  @MaxLength(16)
+  @IsNotEmpty()
   username: string;
 
   @IsString()
   @MinLength(6)
+  @IsNotEmpty()
   @Matches(/(?=.*\d)(?=.*[A-Z])/, {
     message:
       'Password must contain at least one number and one uppercase letter',
