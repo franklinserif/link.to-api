27d86a726eae3018453aa3d1bb4b10c614dc4e44
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:36:35 -0400

    feat(main): add validation for global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { RequestMethod } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.setGlobalPrefix('api', {
     exclude: [{ path: 'links/:id', method: RequestMethod.GET }],
   });
