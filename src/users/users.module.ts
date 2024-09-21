0ebf2ad7bde5f01c7b7d44959fb039c1264b9d18
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:53:57 -0400

    feat(users.module): imports new modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '@users/users.service';
 import { UsersController } from '@users/users.controller';
 import { User } from '@users/entities/user.entity';
+import { AuthModule } from '@auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
 })
 export class UsersModule {}
