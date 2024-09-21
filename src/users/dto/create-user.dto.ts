46786985460d0f14137fdf7fcadc96235ad60043
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:46:38 -0400

    refactor(dtos): add more validation for create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,23 +1,39 @@
-import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @MinLength(2)
+  @MaxLength(16)
+  @IsNotEmpty()
   username: string;
 
   @IsString()
   @MinLength(2)
+  @MaxLength(15)
+  @IsNotEmpty()
   firstName: string;
 
   @IsString()
   @MinLength(2)
+  @MaxLength(15)
+  @IsNotEmpty()
   lastName: string;
 
   @IsEmail()
+  @IsNotEmpty()
+  @MinLength(4)
   email: string;
 
   @IsString()
   @MinLength(6)
+  @IsNotEmpty()
   @Matches(/(?=.*\d)(?=.*[A-Z])/, {
     message:
       'Password must contain at least one number and one uppercase letter',
