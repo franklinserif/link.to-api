9bfccd912353994496f940a157c8f0291fd97f35
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:45:48 -0400

    refactor(imports more modules):

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -4,10 +4,11 @@ import { LinksController } from './links.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Link } from '@links/entities/link.entity';
 import { VisitsModule } from '@visits/visits.module';
+import { AuthModule } from '@auth/auth.module';
 
 @Module({
   controllers: [LinksController],
   providers: [LinksService],
-  imports: [TypeOrmModule.forFeature([Link]), VisitsModule],
+  imports: [TypeOrmModule.forFeature([Link]), VisitsModule, AuthModule],
 })
 export class LinksModule {}
