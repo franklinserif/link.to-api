673249d24a0169748cf88f5da4edb2e48ef97ece
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:56:12 -0400

    feat(auth.module): add typeorm and jwt implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
 import { AuthController } from '@auth/auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from '@auth/strategies/jwt-strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@users/entities/user.entity';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => {
+        return {
+          secret: process.env.PASSPORT_SECRET,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
+  ],
+  exports: [JwtStrategy, PassportModule, JwtModule, AuthService],
 })
 export class AuthModule {}
