7a924ef11e66f4fb1534719fe49daea68fbe5834
franklin rodriguez <franklinserif@gmail.com>
2024-09-21 13:43:41 -0400

    feat(links.controller): add GetUser for create

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -13,14 +13,19 @@ import { Response, Request } from 'express';
 import { LinksService } from '@links/links.service';
 import { CreateLinkDto, UpdateLinkDto } from '@links/dto';
 import { getVisitorInformation } from '@libs/visitor';
+import { GetUser } from '@auth/decorators/get-user.decorator';
+import { User } from '@users/entities/user.entity';
 
 @Controller('links')
 export class LinksController {
   constructor(private readonly linksService: LinksService) {}
 
   @Post()
-  create(@Body() createLinkDto: CreateLinkDto) {
-    return this.linksService.create(createLinkDto);
+  async create(
+    @Body() createLinkDto: CreateLinkDto,
+    @GetUser() user: User | undefined,
+  ) {
+    return await this.linksService.create(createLinkDto, user);
   }
 
   @Get()
