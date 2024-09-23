3510fd360c27c923d8a88f96e2b9d6fd7ab6679c
franklin rodriguez <franklinserif@gmail.com>
2024-09-23 04:54:34 -0400

    chore(.env.template and config/configuration): add new environments variable

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,7 @@
 export default () => ({
   port: parseInt(process.env.SERVER_PORT, 10) || 3000,
   passportSecret: process.env.PASSPORT_SECRET || '1234567',
+  env: process.env.NODE_ENV || 'development',
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
