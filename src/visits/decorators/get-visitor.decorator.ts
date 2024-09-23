fd743a1c7280de54826dabce81dcd7ba8077171c
franklin rodriguez <franklinserif@gmail.com>
2024-09-23 05:42:44 -0400

    feat(visitors/decorators): add get-visitor

---
+++ src/visits/decorators/get-visitor.decorator.ts
@@ -0,0 +1,19 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { getVisitorInformation } from '@libs/visitor';
+
+export const GetVisitor = createParamDecorator(
+  async (_: string, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+    try {
+      const visitor = await getVisitorInformation(req);
+      return visitor;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('cannot get visitor information');
+    }
+  },
+);
