2278092bae6e1a5a4f886585855640e4782bf1f3
franklin rodriguez <franklinserif@gmail.com>
2024-09-23 11:53:12 -0400

    build(implement swagger):

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,15 @@ async function bootstrap() {
   app.setGlobalPrefix('api', {
     exclude: [{ path: 'links/:id', method: RequestMethod.GET }],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('linkto API')
+    .setDescription('restfull api for linkto')
+    .setVersion('1.0')
+    .addTag('linkto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
