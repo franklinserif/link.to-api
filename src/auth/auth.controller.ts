f57502de43f34588804f2791382b5455421d5809
franklin rodriguez <franklinserif@gmail.com>
2024-09-23 04:58:50 -0400

    refactor(auth.controller): improve implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,64 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from '@auth/auth.service';
+import { ConfigService } from '@nestjs/config';
 import { SignInDto } from '@auth/dto';
 import { CreateUserDto } from '@users/dto';
+import { PRODUCTION } from '@shared/constants/server';
+import { GetRefreshToken } from './decorators/get-refreshtoken.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('signin')
-  async signIn(@Body() credentials: SignInDto) {
-    return await this.authService.signIn(credentials);
+  async signIn(@Body() credentials: SignInDto, @Res() res: Response) {
+    const tokens = await this.authService.signIn(credentials);
+    const { accessToken, refreshToken } = tokens;
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === PRODUCTION,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.send({ accessToken });
   }
 
   @Post('signup')
-  async signUp(@Body() createUserDto: CreateUserDto) {
-    return await this.authService.signUp(createUserDto);
+  async signUp(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    const result = await this.authService.signUp(createUserDto);
+
+    const { accessToken, refreshToken, user } = result;
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === PRODUCTION,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.send({ accessToken, user });
+  }
+
+  @Get('refresh-token')
+  async refreshToken(
+    @GetRefreshToken() refreshToken: string,
+    @Res() res: Response,
+  ) {
+    const newTokens = await this.authService.refreshToken(refreshToken);
+
+    res.cookie('refreshToken', newTokens.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === PRODUCTION,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.send({ accessToken: newTokens.accessToken });
   }
 }
