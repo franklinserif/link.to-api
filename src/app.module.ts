cd0596a6e67f902ac9fe906edeac9046fca4c8c5
franklin rodriguez <franklinserif@gmail.com>
2024-09-24 12:53:45 -0400

    chore(app.module): remove AppController and AppService imports
    it's necessary anymore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
