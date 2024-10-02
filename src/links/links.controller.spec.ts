a0ea2c75992a11d1afb7a8824fd4b36d4fb6e1d0
franklin rodriguez <franklinserif@gmail.com>
2024-10-02 16:46:57 -0400

    chore(links.controller): add passing test

--- src/links/links.controller.spec.ts
+++ src/links/links.controller.spec.ts
@@ -5,23 +5,25 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Link } from '@links/entities/link.entity';
 import { VisitsService } from '@visits/visits.service'; // Asegúrate de importar el servicio adecuado
 import { PassportModule } from '@nestjs/passport';
+import { VisitorInformation } from '@shared/interfaces/visitor';
+import { LINKS } from '@shared/constants/testVariables';
 
 describe('LinksController', () => {
   let controller: LinksController;
-  //let service: LinksService;
+  let service: LinksService;
+  const visitor: VisitorInformation = {
+    userAgent: {
+      browser: 'chrome',
+      os: 'Linux' as unknown as UAParser.IOS,
+    },
+    geo: {
+      ip: '192.168.123.132',
+      location: 'san francisco',
+      country: 'EEUU',
+    },
+  };
 
   beforeEach(async () => {
-    const mockLinkRepository = {
-      find: jest.fn(),
-      findOne: jest.fn(),
-      save: jest.fn(),
-      remove: jest.fn(),
-    };
-
-    const mockVisitsService = {
-      trackVisit: jest.fn(), // Simula cualquier método que utilices del VisitsService
-    };
-
     const module: TestingModule = await Test.createTestingModule({
       imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [LinksController],
@@ -29,17 +31,33 @@ describe('LinksController', () => {
         LinksService,
         {
           provide: getRepositoryToken(Link),
-          useValue: mockLinkRepository,
+          useValue: {
+            findAll: jest.fn(() => LINKS),
+            findOne: jest.fn(),
+            save: jest.fn(),
+            remove: jest.fn(),
+          },
         },
         {
-          provide: VisitsService, // Proporciona el mock del VisitsService
-          useValue: mockVisitsService,
+          provide: VisitsService,
+          useValue: { trackVisit: jest.fn() },
         },
       ],
     }).compile();
 
     controller = module.get<LinksController>(LinksController);
-    // service = module.get<LinksService>(LinksService);
+    service = module.get<LinksService>(LinksService);
+
+    jest
+      .spyOn(service as any, 'checkExpireDate')
+      .mockImplementation(async (link: Link) => {
+        if (link.expirationDate > new Date()) {
+          return link;
+        } else {
+          link.status = false;
+          return link;
+        }
+      });
   });
 
   it('should be defined', () => {
