a21b936692a5745dc3ccc42f7035258cfc9437fe
franklin rodriguez <franklinserif@gmail.com>
2024-10-07 19:03:07 -0400

    refactor(mails.service): implement the template

--- src/mails/mails.service.ts
+++ src/mails/mails.service.ts
@@ -1,5 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { render } from '@react-email/render';
+import Welcome from './templates/welcome';
 import { ErrorManager } from '@shared/exceptions/ExceptionManager';
 
 @Injectable()
@@ -7,18 +9,18 @@ export class MailsService {
   constructor(private readonly mailerService: MailerService) {}
 
   async sendMail() {
+    const template = await render(Welcome());
     try {
-      const message = 'Forgot your password?';
-
       await this.mailerService.sendMail({
         from: 'Kingsley Okure <kingsleyokgeorge@gmail.com>',
         to: 'franklinserif@gmail.com',
         subject: 'reset password test',
-        text: message,
+        html: template,
       });
 
       return { message: 'mail was sent' };
     } catch (error) {
+      console.log('error: ', error);
       throw new ErrorManager(error);
     }
   }
