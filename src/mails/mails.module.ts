4afa72361d48a6106cb81c9975a579dd8a273c70
franklin rodriguez <franklinserif@gmail.com>
2024-10-07 17:44:32 -0400

    feat(module): add mails.module

---
+++ src/mails/mails.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailsService } from '@mails/mails.service';
+import { MailsController } from '@mails/mails.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  controllers: [MailsController],
+  providers: [MailsService],
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('MAIL_HOST'),
+          port: configService.get<number>('MAIL_PORT'),
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASSWORD'),
+          },
+        },
+      }),
+    }),
+  ],
+  exports: [MailsService],
+})
+export class MailsModule {}
