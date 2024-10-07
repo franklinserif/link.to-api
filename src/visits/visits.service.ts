f0f54be8488576a3eb798c91c172d053b3da2924
franklin rodriguez <franklinserif@gmail.com>
2024-10-07 16:21:46 -0400

    refactor(visits.service): remove unecessary imports

--- src/visits/visits.service.ts
+++ src/visits/visits.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateVisitDto } from '@visits/dto';
