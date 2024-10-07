7229f4e8d6ddc566cd1a188996ac7a9ae63040aa
franklin rodriguez <franklinserif@gmail.com>
2024-10-07 17:44:46 -0400

    chore(config): add new environment variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,12 @@ export default () => ({
   port: parseInt(process.env.SERVER_PORT, 10) || 3000,
   passportSecret: process.env.PASSPORT_SECRET || '1234567',
   env: process.env.NODE_ENV || 'development',
+  mail: {
+    host: process.env.MAIL_HOST || 'sandbox.smtp.mailtrap.io',
+    port: +(process.env.MAIL_PORT || '2525'),
+    user: process.env.MAIL_USER || 'franklinserif',
+    password: process.env.MAIL_PASSWORD || '1234567',
+  },
   database: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
