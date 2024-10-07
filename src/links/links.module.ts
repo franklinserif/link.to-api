d3d97634b4a8b8e3c1027114aa0244440c336c85
franklin rodriguez <franklinserif@gmail.com>
2024-10-06 20:00:52 -0400

    feat(links.module): import schedule module

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { LinksService } from '@links/links.service';
 import { LinksController } from '@links/links.controller';
 import { Link } from '@links/entities/link.entity';
@@ -9,7 +10,12 @@ import { AuthModule } from '@auth/auth.module';
 @Module({
   controllers: [LinksController],
   providers: [LinksService],
-  imports: [TypeOrmModule.forFeature([Link]), VisitsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Link]),
+    ScheduleModule.forRoot(),
+    VisitsModule,
+    AuthModule,
+  ],
   exports: [LinksService],
 })
 export class LinksModule {}
